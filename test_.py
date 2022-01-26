1f147f0c22bf6cb7e06f4a9ac03ea4c6f1e7f732
Biel Font <biel.font@inphower.com>
2022-01-26 20:52:20 +0100

    circle11

--- test_.py
+++ test_.py
@@ -5,3 +5,8 @@ def test_operaciones():
     assert operaciones.suma(3, 3) == 6
     assert operaciones.suma(-3, 3) == 0
     assert operaciones.suma(-3, -3) == -6
+
+def test_operaciones():
+    assert operaciones.resta(3, 3) == 0
+    assert operaciones.resta(3, 2) == 1
+    assert operaciones.resta(3, 0) == 3
